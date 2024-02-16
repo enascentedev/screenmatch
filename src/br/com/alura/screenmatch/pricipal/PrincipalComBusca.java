1e9486be1dfb91ac7bc9b1f841e31fc7d5dcfc2c
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2024-02-15 21:43:13 -0300

    feat: criando chamada api de filmes

---
+++ src/br/com/alura/screenmatch/pricipal/PrincipalComBusca.java
@@ -0,0 +1,17 @@
+package br.com.alura.screenmatch.pricipal;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+public class PrincipalComBusca {
+    public static void main (String[] args) throws IOException, InterruptedException, IOException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=1352f28e"))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+}
