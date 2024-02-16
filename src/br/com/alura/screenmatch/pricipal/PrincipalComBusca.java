f73f7f1dc6911207194c815d6df84e8c40d25135
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2024-02-15 22:00:57 -0300

    wip: deixando busca dinamica

--- src/br/com/alura/screenmatch/pricipal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/pricipal/PrincipalComBusca.java
@@ -4,11 +4,19 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
+
 public class PrincipalComBusca {
     public static void main (String[] args) throws IOException, InterruptedException, IOException {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite um filme pra a busca");
+        var busca= leitura.nextLine();
+
+        String endereco= "https://www.omdbapi.com/?t=" + busca + "&apikey=1352f28e";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=1352f28e"))
+                .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
